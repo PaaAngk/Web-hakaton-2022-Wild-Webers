365627711a00a0459d7b62a22d06417bb36aa3fb
Deduch <paaAngarsk@yandex.ru>
2022-05-22 19:44:27 +0800

    add backgroud

--- src/app/modules/timeTable/timeTable.component.ts
+++ src/app/modules/timeTable/timeTable.component.ts
@@ -1,3 +1,4 @@
+import { Schedules } from './../../core/modules/schedules.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SchedulesService } from 'src/app/core/services';
@@ -10,6 +11,7 @@ import {ActivitiesService} from 'src/app/core/services/activities.service';
   styleUrls: ['./timeTable.component.scss']
 })
 export class TimeTableComponent implements OnInit {
+  tableGroupData?: Array<Schedules>
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -24,13 +26,7 @@ export class TimeTableComponent implements OnInit {
   }
 
   clickButton(){
-
-    //this.schedulesService.get().subscribe(data=>( console.log(data)))
     const d: Date = new Date();
-    this.schedulesService.getGroup("Муми-тролли","2022-05-23").subscribe(data=>( console.log(data)))
-    //this.schedulesService.getTeachers("Морра","2022-04-11").subscribe(data=>( console.log(data)))
-    //this.schedulesService.getAuditories("Танцплощадка","2022-04-11").subscribe(data=>( console.log(data)))
-    
-
+    this.schedulesService.getGroup("Муми-тролли","2022-05-23").subscribe(data=>( console.log(data), this.tableGroupData = data))
   }
 }
