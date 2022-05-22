d0412457b7d8fbf89ab31daa3b0c8ecbe8a7dd5c
KomogortsevaYulia <komog-y-v@yandex.ru>
2022-05-22 23:37:11 +0800

    Исправила стиль

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Schedules } from './../../core/modules/schedules.model';
+import { SchedulesService } from 'src/app/core/services';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-
-  constructor() { }
+  schedules: Schedules[]=[];
+  tableGroupData?: Array<Schedules> =[]
+  constructor(private schedulesService: SchedulesService) { }
 
   ngOnInit() {
   }
+  clickButton() {
+    const d: Date = new Date();
 
+    this.schedulesService.getGroup("Муми-тролли","2022-05-23").subscribe(data => this.tableGroupData=data);
+    //this.schedulesService.getTeachers("Морра","2022-04-11").subscribe(data=>( console.log(data)))
+    //this.schedulesService.getAuditories("Танцплощадка","2022-04-11").subscribe(data=>( console.log(data)))
+
+    console.log(this.schedulesService.listGroup);
+    console.log(this.schedulesService.listAuditories);
+    console.log(this.schedulesService.listTeachers);
+  }
 }
