513797266f9645ee623aa93d88b26395ac9fafd2
KomogortsevaYulia <komog-y-v@yandex.ru>
2022-05-26 23:08:03 +0800

    Сделала верстку

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -19,7 +19,7 @@ export class ApiService {
   }
 
   get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
-    return this.http.get('https://equable-dot-somersault.glitch.me'+path, { params })
+    return this.http.get('https://rain-harsh-zebu.glitch.me'+path, { params })
       .pipe(catchError(this.formatErrors));
   }
 }
